599c8c8b98ab478fe6fd8cac370d482cdfca42ae
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-21 23:46:11 +0800

    11:46

--- app/Controllers/CoachDashboardController.php
+++ app/Controllers/CoachDashboardController.php
@@ -150,7 +150,7 @@ class CoachDashboardController extends BaseController
             $model = new TimeScheduleModel(); // Change to your actual model name
             $data['time'] = $model->findAll(); // Fetch all schedules from the database
             
-            return view('/coachdashboard/TimeScheds', $data);
+            return view('/coachdashboard/TimeSheds', $data);
         }
         public function timestore()
         {

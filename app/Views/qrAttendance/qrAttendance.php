669d1a4a05fae52b7c63d3c113f82f2e39c2fcfc
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-23 02:24:43 +0800

    2amqar

--- app/Views/qrAttendance/qrAttendance.php
+++ app/Views/qrAttendance/qrAttendance.php
@@ -81,9 +81,9 @@ $this->section('body'); // Start the body section
 
         if (response.status === 'success') {
             if (response.action === 'checkin') {
-                alert("Check-In Successful for Customer ID: " + response.customerID);
+                alert("Check-In Successful for Customer ID: " + $("#userId").text(customer.CustomerID || "N/A"));
             } else if (response.action === 'checkout') {
-                alert("Check-Out Successful for Customer ID: " + response.customerID);
+                alert("Check-Out Successful for Customer ID: " + response.CustomerID);
             }
             // Optionally reload the table
         } else {

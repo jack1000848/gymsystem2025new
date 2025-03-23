849656b75b6930639a2a32f208f5538ce04a9676
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-24 00:24:04 +0800

    coachviewplanmodel

--- app/Models/CoachPlanView.php
+++ app/Models/CoachPlanView.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class CoachPlanView extends Model
 {
-    protected $table            = 'CoachPlanView';  // The name of the view
+    protected $table            = 'coachplanview';  // The name of the view
     protected $primaryKey       = 'CoachID';  // Optional, if the view has a unique identifier like CoachID
     protected $useAutoIncrement = false;  // Views don't typically auto-increment
     protected $returnType       = 'array';  // You want to return data as an array
@@ -15,7 +15,7 @@ class CoachPlanView extends Model
     
     // Define the fields you will be fetching from the view
     protected $allowedFields    = [
-        'CoachID', 'PlanID', 'FullName'  // Include fields that exist in the view
+        'PlanID', 'CoachID', 'FullName'  // Include fields that exist in the view
     ];
     
     // Since you're only fetching data, you don't need insert/save logic

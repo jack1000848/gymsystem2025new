64bf496b656100a142a5d95f681a943b9e8c98bd
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-23 15:20:44 +0800

    qr10

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -133,11 +133,12 @@ $routes->get('/viewequipment', 'ViewEquipmentController::indexviewequipment');
 
 
 ////qr code showing 
-$routes->get('scan-qr', 'QrAttendanceController::viewqrcode');
+//$routes->get('scan-qr', 'QrAttendanceController::viewqrcode');
 //$routes->post('scan-qr/save/(:num)', 'QrAttendanceController::save/$1');
 //$routes->get('scan-qr/save/(:any)', 'QrAttendanceController::save/$1');
 //$routes->post('/scan-qr/delete/(:num)', 'QrAttendanceController::delete/$1');
 $routes->post('scan-qr/save/(:any)', 'QrAttendanceController::save/$1');
+$routes->get('scan-qr', 'QrAttendanceController::viewqrcode');
 //$routes->post('scan-qr/save/', 'QrAttendanceController::list');
 
 ///attendance for tapping qr

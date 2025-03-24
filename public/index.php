cd28d295d37b8203957e3e68b797d736edb7b268
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-24 17:15:47 +0800

    canceltrying

--- public/index.php
+++ public/index.php
@@ -31,8 +31,7 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
  */
 
 // Path to the front controller (this file)
-define('CI_ENVIRONMENT', 'development');
-
+define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
 // Ensure the current directory is pointing to the front controller's directory
 if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {

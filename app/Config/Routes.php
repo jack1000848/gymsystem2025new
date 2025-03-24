06922bb5770603724c6ed22a4766bd777fed6e7b
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-25 03:16:16 +0800

    1

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/admin', 'Admin::index',['filter' => 'login']);
+$routes->get('/admin', 'Admin::index');
 $routes->get('/logout', 'Admin::logout');
 
 $routes->get('/joinus', 'Home::joinus');

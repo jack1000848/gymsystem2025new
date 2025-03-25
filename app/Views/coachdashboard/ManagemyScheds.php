668fd6b21563a5b8dfb4d46878831678e80a5383
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-25 23:24:18 +0800

    56

--- app/Views/coachdashboard/ManagemyScheds.php
+++ app/Views/coachdashboard/ManagemyScheds.php
@@ -11,6 +11,11 @@
 
 </div>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"/>
+    <!-- Or for RTL support -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css"/>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
 <?php if (session()->getFlashdata('success')) :?>

d9acc943bf11ff5ed961037889a7aaa11386c0ed
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-26 15:03:22 +0800

    sira1l

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -131,8 +131,8 @@ $routes->get('/coachdashboard', 'CoachDashboardController::index');
 
 /// Manage my Schedule
 ///Manage Coach... (coach,(edit,update,delete,store)) ////
-$routes->get('/coach-manage', 'CoachDashboardController::coachManage' );
-$routes->post('coach-manage/store', 'CoachDashboardController::storemanage'  ,['filter' => 'login']);
+$routes->get('/coach-manage', 'CoachDashboardController::coachManage', ['filter' => 'login']);
+$routes->post('coach-manage/store', 'CoachDashboardController::storemanage');
 $routes->get('coach-manage/store', 'CoachDashboardController::storemanage');
 $routes->get('/coach-manage/edit/(:num)', 'CoachDashboardController::edit/$1');
 $routes->post('/coach-manage/update/(:num)', 'CoachDashboardController::update/$1');

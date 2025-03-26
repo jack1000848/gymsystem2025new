dbe86ee70eb37a1fabdfca74936556186cd1b9f6
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-26 16:11:43 +0800

    2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -135,7 +135,8 @@ $routes->get('/coach-manage', 'CoachDashboardController::coachManage');
 $routes->post('coach-manage/store', 'CoachDashboardController::storemanage');
 $routes->get('coach-manage/store', 'CoachDashboardController::storemanage');
 $routes->get('/coach-manage/edit/(:num)', 'CoachDashboardController::edit/$1');
-$routes->post('/coach-manage/update', 'CoachDashboardController::update');
+$routes->get('coach-manage/delete/(:num)', 'CoachDashboardController::delete/$1');
+
 $routes->delete('/coach-manage/delete/(:num)', 'CoachDashboardController::delete/$1');
 
 ///View my Clients... and time manage

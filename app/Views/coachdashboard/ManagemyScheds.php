7362322365fe3dfc220a5111d87f81edf7582a72
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-26 13:34:59 +0800

    2

--- app/Views/coachdashboard/ManagemyScheds.php
+++ app/Views/coachdashboard/ManagemyScheds.php
@@ -80,7 +80,7 @@
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
   </div>
   <div class="modal-body">
-  <form id="planForm" action = "<?= site_url('coach-manage/store'); ?>" method="POST">
+  <form id="planForm" method="POST">
 
  <div class="mb-3">
      <label for="exampleFormControlInput1" class="form-label">Start Date:</label>

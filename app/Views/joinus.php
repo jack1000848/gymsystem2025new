620273925ad3567c712264f090095b054c6013af
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-28 02:59:56 +0800

    3

--- app/Views/joinus.php
+++ app/Views/joinus.php
@@ -39,10 +39,14 @@
                 <h1>Admin Login</h1>
             </div>
             <form action="<?= base_url('admin-login') ?>" method="post">
-    <input type="text" name="username" id="username" placeholder="Username" required>
-    <input type="password" name="password" id="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
+                <input type="text" name="username" id="username" placeholder="Username" required>
+                 <input type="password" name="password" id="password" placeholder="Password" required>
+                 <button type="submit">Login</button>
+                <a href="<?= base_url('join-now') ?>" class="register">Signup</a>
+                <a href="<?= base_url('member-login') ?>" class="register">Member Login</a>
+                <a href="<?= base_url('coach-login') ?>" class="register">Coach Login</a>
+                
+            </form>
         </div>
     </div>
 </body>

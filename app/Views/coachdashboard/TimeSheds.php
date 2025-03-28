68790e1056bd03d38ef56cad5c42dccdaa5df0ac
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-29 00:19:09 +0800

    1

--- app/Views/coachdashboard/TimeSheds.php
+++ app/Views/coachdashboard/TimeSheds.php
@@ -38,11 +38,12 @@
 <?php foreach ($coach as $coachSched): ?>
 
 <tr>
-<th scope="row"><?= $coachSched->ScheduleDate; ?></th>
-    <td><?= $coachSched->Start; ?></td>
-    <td><?= $coachSched->End; ?></td>
-    <td><?= $coachSched->CustomerName; ?></td>
-    <td><?= $coachSched->Day; ?></td>
+<<th scope="row"><?= $coachSched['ScheduleDate']; ?></th>
+<td><?= $coachSched['Start']; ?></td>
+<td><?= $coachSched['End']; ?></td>
+<td><?= $coachSched['CustomerName']; ?></td>
+<td><?= $coachSched['Day']; ?></td>
+
  
 
 <td>

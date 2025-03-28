0970d1e6dc027cb374430e0966f78d5b33bef1a6
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-29 01:17:44 +0800

    1

--- app/Controllers/LoginClientController.php
+++ app/Controllers/LoginClientController.php
@@ -22,9 +22,9 @@ use App\Models\CustomerModel;
 
     public function authenticate()
 { 
-    if (!session()->has('isLoggedIn')) { // Dito dapat ang check, hindi sa login function
-        return redirect()->to('/member-login')->with('error', 'Please login first.');
-    }
+   // if (!session()->has('isLoggedIn')) { // Dito dapat ang check, hindi sa login function
+      //  return redirect()->to('/member-login')->with('error', 'Please login first.');
+   // }
     $session = session();
     $customerModel = new CustomerModel();
 

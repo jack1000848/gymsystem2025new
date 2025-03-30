dcde817c5f50b0f1f99a0a1233d03c63055534af
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-30 16:18:47 +0800

    2

--- app/Views/qrAttendance/qrAttendance.php
+++ app/Views/qrAttendance/qrAttendance.php
@@ -156,7 +156,7 @@ $this->section('body'); // Start the body section
         let userType = document.getElementById("userType").value;
 
         if (userType === "client") {
-            window.location.href = "client_dashboard.php"; // Change to your actual client view
+            window.location.href = "scan-qr"; // Change to your actual client view
         } else if (userType === "coach") {
             window.location.href = "coachattendanceqr"; // Change to your actual coach view
         }

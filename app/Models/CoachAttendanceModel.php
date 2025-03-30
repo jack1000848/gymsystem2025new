2943401ee48fadc7ec1fbb2d898838ff6e6e3a2f
jack1000848 <taysonmiguelito125@gmail.com>
2025-03-31 00:56:02 +0800

    1

--- app/Models/CoachAttendanceModel.php
+++ app/Models/CoachAttendanceModel.php
@@ -12,4 +12,36 @@ class CoachAttendanceModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $allowedFields    = ['id','CoachID', 'CheckInTime', 'CheckOutTime'];
+
+    
+
+protected bool $allowEmptyInserts = false;
+protected bool $updateOnlyChanged = true;
+
+protected array $casts = [];
+protected array $castHandlers = [];
+
+// Dates
+protected $useTimestamps = false;
+protected $dateFormat    = 'datetime';
+protected $createdField  = 'created_at';
+protected $updatedField  = 'updated_at';
+protected $deletedField  = 'deleted_at';
+
+// Validation
+protected $validationRules      = [];
+protected $validationMessages   = [];
+protected $skipValidation       = false;
+protected $cleanValidationRules = true;
+
+// Callbacks
+protected $allowCallbacks = true;
+protected $beforeInsert   = [];
+protected $afterInsert    = [];
+protected $beforeUpdate   = [];
+protected $afterUpdate    = [];
+protected $beforeFind     = [];
+protected $afterFind      = [];
+protected $beforeDelete   = [];
+protected $afterDelete    = [];
 }

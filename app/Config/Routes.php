db7967ee1c1f878f5f7f767e9a7f0da74e536c54
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-01 12:57:34 +0800

    adding /$1 routes renew

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -77,8 +77,8 @@ $routes->get('clients1/renew', 'CustomerController::renew');
 ///BUTTONS/// Freeze | View | renew \\\\\\
 $routes->post('/customer/toggleFreeze/(:num)', 'CustomerController::toggleFreeze/$1');
 $routes->get('/clients1/view/(:num)', 'CustomerController::viewClient/$1');
-$routes->get('/clients1/renew', 'CustomerController::renew');
-$routes->post('/clients1/renew/(:num)', 'CustomerController::renew');
+$routes->get('/clients1/renew/(:num)', 'CustomerController::renew/$1');
+$routes->post('/clients1/renew/(:num)', 'CustomerController::renew/$1');
 
 ///Manage Coach... (coach,(edit,update,delete,store)) ////
 $routes->get('/coach', 'CoachController::index' );

0b1c613049d63970bbfcfc0d0f466e52b19f16af
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-01 13:38:42 +0800

    1

--- app/Views/clients1crud/manage1.php
+++ app/Views/clients1crud/manage1.php
@@ -309,7 +309,7 @@ $this->section('body'); // Start the body section
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="renewClientModalLabel">Edit Client</h1>
+                <h1 class="modal-title fs-5" id="renewClientModalLabel">renew Client</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">

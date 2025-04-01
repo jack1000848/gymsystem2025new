352fb41bff5e124e53af6e80e16ae4d5fc259636
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-01 15:27:35 +0800

    1

--- app/Views/clients1crud/manage1.php
+++ app/Views/clients1crud/manage1.php
@@ -386,7 +386,7 @@ $this->section('body'); // Start the body section
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     $(document).ready(function () {
         // Initialize DataTable

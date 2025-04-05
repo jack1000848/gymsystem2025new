81f5ee9a2c2fdaf0fd9233628c33c0f54f5d3a0a
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-05 12:54:06 +0800

    1

--- app/Controllers/CoachDashboardController.php
+++ app/Controllers/CoachDashboardController.php
@@ -186,7 +186,7 @@ public function accountSettings()
             return redirect()->to('/dashboard')->with('error', 'User not found.');
         }
 
-        return view('coachdashboard/accountsettings', ['user' => $user]);
+        return view('coachdashboard/accountsettings1', ['user' => $user]);
     }
 
     public function updateAccount()

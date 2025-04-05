7dc48a2dfa4fae3edac3c8a77afaa2de8416d795
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-05 17:18:21 +0800

    1

--- app/Views/qrAttendance/qrAttendance.php
+++ app/Views/qrAttendance/qrAttendance.php
@@ -9,7 +9,7 @@ $this->section('body'); // Start the body section
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SCAN YOUR QR CODE </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+   
 
     <script src="https://unpkg.com/html5-qrcode"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>

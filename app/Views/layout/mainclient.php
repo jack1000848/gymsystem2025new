a5d7308b86dd95b63d681190c40d06008c0d7060
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-05 13:50:33 +0800

    1

--- app/Views/layout/mainclient.php
+++ app/Views/layout/mainclient.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <style>
         

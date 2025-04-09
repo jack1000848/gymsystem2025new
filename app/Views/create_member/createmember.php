251f164b33e0c95c38857ea4c88f570dec17ab51
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-09 21:23:29 +0800

    1

--- app/Views/create_member/createmember.php
+++ app/Views/create_member/createmember.php
@@ -86,27 +86,24 @@
 
 </div>
 
-<div class="mb-3" id="coachSelectDiv">
-<label for="coach" class="form-label">Select Coach</label>
-<select id="coach" class="form-control" name="coach" required>
-    <option value="">Select a Coach</option>
-</select>
+    <div class="mb-3" id="coachSelectDiv">
+        <label for="coach" class="form-label">Select Coach</label>
+        <select id="coach" class="form-control" name="coach" required>
+      <option value="">Select a Coach</option>
+        </select>
+    </div>
+            <!-- Coach Sched -->    
+            <div class="mb-3" id="coachschedSelectDiv">
+                                    <label for="coachsched" class="form-label">Select Schedules</label>
+                                    <select id="coachsched" class="form-control" multiple name="coachsched" required>
+                                        <option value="">Select a Coach</option>
+                                    </select>
+            </div>
 
-</div>
- <!-- Coach Sched -->    
- <div class="mb-3" id="coachschedSelectDiv">
-                        <label for="coachsched" class="form-label">Select Schedules</label>
-                        <select id="coachsched" class="form-control" multiple name="coachsched" required>
-                            <option value="">Select a Coach</option>
-                        </select>
-  </div>
-
-<div class="mb-3">
-    <label for="amount" class="form-label">Total Amount</label>
-    <input type="text" id="priceInput" class="form-control" name="amount" readonly>
-</div>
-
-
+        <div class="mb-3">
+            <label for="amount" class="form-label">Total Amount</label>
+         <input type="text" id="priceInput" class="form-control" name="amount" readonly>
+        </div>
 
     <div class="mb-3">
         <label for ="duration" class="form-label">Duration </label>
@@ -229,8 +226,11 @@ async function fetchPlans() {
     } catch (error) {
         console.error("Error fetching plans:", error);
     }
-}
+    }
 
  </script>
+</div>
+        </div>
+    </div>
 </body>
 </html>

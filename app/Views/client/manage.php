2555ff4b57cc43203bd41f96fcb450a53b700b8e
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-10 11:03:00 +0800

    bakit ayaw

--- app/Views/client/manage.php
+++ app/Views/client/manage.php
@@ -146,7 +146,10 @@
          <label for="editClients1lname" class="form-label">Last Name</label>
              <input type="text" id="editClients1lname" class="form-control" name="Lastname"required>
 </div>
-
+<div class="mb-3">
+         <label for="editemail" class="form-label">Email</label>
+              <input type="text" id="editemail" class="form-control" name="Email"required>
+    </div> 
 <div class="mb-3">
         <label for="editpassword" class="form-label">Password</label>
         <input type="text" id="editpassword" class="form-control" name="Password" required>
@@ -157,13 +160,6 @@
          <label for="editaddress" class="form-label">Address</label>
               <input type="text" id="editaddress" class="form-control" name="Address"required>
     </div> 
-
-    <div class="mb-3">
-         <label for="editemail" class="form-label">Email</label>
-              <input type="text" id="editemail" class="form-control" name="Email"required>
-    </div> 
-
-    
 
       </div>
       <div class="modal-footer">

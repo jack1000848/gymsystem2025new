cf40683c058076b76022def9eb7bebcb456111b2
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-10 20:40:52 +0800

    making client dashboard for client

--- app/Views/layout/mainclient.php
+++ app/Views/layout/mainclient.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>iSFGYM | Coach | Dashboard</title>
+    <title>iSFGYM | Client | Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Fonts and Styles -->

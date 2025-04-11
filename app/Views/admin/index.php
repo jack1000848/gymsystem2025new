4ba02bbcef06a9e056926c1f5eeb799a2fd2a796
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-11 14:26:35 +0800

    1

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -110,7 +110,7 @@
         <div class="card shadow-sm p-3 mb-5 bg-white rounded">
             <div class="card-body">
                 <h5 class="card-title text-center">Gender Chart</h5>
-                <div id="donut_chart" style="width: 100%; height: 300px;"></div>
+                <div id="donut_chart" style="width: 150%; height: 350px;"></div>
             </div>
         </div>
     </div>

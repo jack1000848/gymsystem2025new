f1030a737a82513af27cbb09b8f90a35103717f0
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-12 12:58:53 +0800

    try

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -348,7 +348,7 @@
     <div class="col-md-6">
         <div class="card shadow-sm p-3 mb-5 bg-white rounded h-100">
             <div class="card-body">
-                <h5 class="card-title text-center">Gender Chart</h5>
+            <h5 class="card-title chart-title">Gender Chart</h5>
                 <div id="donut_chart" style="width: 100%; height: 350px;"></div>
             </div>
         </div>

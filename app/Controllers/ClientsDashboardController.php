74fabad316e263c94fd9c5045c40b9b656837d35
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-12 16:21:11 +0800

    1

--- app/Controllers/ClientsDashboardController.php
+++ app/Controllers/ClientsDashboardController.php
@@ -32,7 +32,7 @@ class ClientsDashboardController extends BaseController
     $data['client'] = $client;
 
     // If no coach assigned, show a message in the view instead of redirecting
-    if (!$client || !$client->CustomerID) {
+    if (!$client || !$client->CoachID) {
         $data['noCoach'] = true;
         return view('clientdashboard/clientnotif', $data);
     }
@@ -51,7 +51,6 @@ class ClientsDashboardController extends BaseController
 
     return view('clientdashboard/clientnotif', $data);
 }
-
 
     public function index()
     {

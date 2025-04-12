674bfa2ab30c9c4df1b94910628768f53944df1c
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-12 15:36:26 +0800

    1

--- app/Views/layout/mainclient.php
+++ app/Views/layout/mainclient.php
@@ -200,7 +200,12 @@
                                 <p class="m-0">Account Settings</p>
                             </a> 
                         </li>
-                        
+                        <li class="nav-item"> 
+                            <a href="<?= base_url('client-coach-absent') ?>" class="nav-link d-flex align-items-center gap-2"> 
+                                <i class="bi bi-gear nav-icon"></i>
+                                <p class="m-0">Coachotification</p>
+                            </a> 
+                        </li>
                         <li class="nav-item"> 
                             <a href="<?= base_url('/logout') ?>"class="nav-link d-flex align-items-center gap-2"> 
                             <i class="bi bi-box-arrow-right nav-icon"></i>

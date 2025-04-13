59e9c66c71596decbe1019acd4a2e46487be8604
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-13 21:15:07 +0800

    fixed

--- app/Views/coachdashboard/accountsettings1.php
+++ app/Views/coachdashboard/accountsettings1.php
@@ -21,7 +21,7 @@
 
         form {
             background-color: #ffffff;
-            
+            padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0,0,0,0.05);
             max-width: 500px;

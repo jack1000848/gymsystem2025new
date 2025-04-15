546564c63a2262e11480e9092171292b38ae354e
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-15 18:23:26 +0800

    1

--- app/Views/coachdashboard/assigntask.php
+++ app/Views/coachdashboard/assigntask.php
@@ -28,11 +28,15 @@
                     <label for="CustomerID" class="form-label">Select Client</label>
                     <select name="CustomerID" id="CustomerID" class="form-select" required>
                         <option value="">-- Select a Client --</option>
-                        <?php foreach ($customers as $customer): ?>
-                            <option value="<?= $customer['CustomerID'] ?>" <?= old('CustomerID') == $customer['CustomerID'] ? 'selected' : '' ?>>
-                                <?= esc($customer['Firstname'] . ' ' . $customer['Lastname']) ?>
-                            </option>
-                        <?php endforeach; ?>
+                        <?php if (isset($customers) && !empty($customers)): ?>
+                            <?php foreach ($customers as $customer): ?>
+                                <option value="<?= esc($customer['CustomerID']) ?>" <?= old('CustomerID') == $customer['CustomerID'] ? 'selected' : '' ?>>
+                                    <?= esc($customer['Firstname'] . ' ' . $customer['Lastname']) ?>
+                                </option>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <option value="" disabled>No clients available</option>
+                        <?php endif; ?>
                     </select>
                 </div>
 

a6dacc23e3c487ce34141170327b6326f4f8d4df
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-16 14:25:17 +0800

    1

--- app/Views/gymplan/manageplan.php
+++ app/Views/gymplan/manageplan.php
@@ -62,7 +62,7 @@ $this->section('body');
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="addPlanForm">
+                <form id="addPlanForm" action="<?= site_url('/gymplan/store'); ?>" method="POST">>
                     <div class="mb-3">
                         <label for="planName" class="form-label">Plan Name</label>
                         <input type="text" class="form-control" name="Pname" id="planName" required>
@@ -95,7 +95,7 @@ $this->section('body');
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-primary">Save Plan</button>
                     </div>
-                </form>
+                </foid=>
             </div>
         </div>
     </div>

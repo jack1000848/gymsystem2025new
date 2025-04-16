cd01a126ccb924ea2accd966d221a9c758d10859
jack1000848 <taysonmiguelito125@gmail.com>
2025-04-17 00:33:26 +0800

    1

--- app/Views/equipment/eqmanage.php
+++ app/Views/equipment/eqmanage.php
@@ -3,6 +3,8 @@
     $this ->section('body');
 
     ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
     body {
         background-color: #f4f4f4;

f59a9782ab0e85cb6965da24f517bdd33934e6c4
migs <taysonmiguelito125@gmail.com>
2025-04-18 14:20:04 +0800

    2

--- app/Views/create_member/createmember.php
+++ app/Views/create_member/createmember.php
@@ -228,6 +228,24 @@
     }
     }
 
+
+    document.getElementById('togglePassword').addEventListener('click', function () {
+            const passwordInput = document.getElementById('password');
+            const toggleIcon = document.getElementById('toggleIcon');
+            
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                toggleIcon.classList.remove('bi-eye');
+                toggleIcon.classList.add('bi-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                toggleIcon.classList.remove('bi-eye-slash');
+                toggleIcon.classList.add('bi-eye');
+            }
+        });
+
+
+
     </script>
    
 </body>

46d273dfd9a2209f65cca7b8fbae6769185b37d1
migs <taysonmiguelito125@gmail.com>
2025-04-19 16:39:53 +0800

    1

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -183,13 +183,8 @@ class CustomerController extends BaseController
             'is_verified' => 0
 
      ]; 
-     $plan = $planModel->find($data['Membership_plan']);
-     if (!$plan) {
-         return $this->response->setJSON([
-             'status' => 'error',
-             'message' => 'Invalid membership plan selected.',
-         ]);
-     }
+     
+     
      $startDate = new \DateTime($data['ExpirationDate']);
      $duration = $data['duration'] ?? 30;
      $endDate = (clone $startDate)->modify("+{$duration} days");

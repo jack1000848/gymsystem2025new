d697d2fe2cecd402cfc8da5493ac1e4406c96a05
migs <taysonmiguelito125@gmail.com>
2025-04-20 01:41:18 +0800

    tanga ata get hinahanap

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -245,6 +245,7 @@ $routes->post('/payment/add', 'paymentController::add');
 $routes->get('/payment/add', 'paymentController::add');
 $routes->match(['get', 'post'], '/payment/edit/(:num)', 'paymentController::edit/$1');
 $routes->post('/payment/update/(:num)', 'paymentController::update/$1');
+$routes->get('/payment/update/(:num)', 'paymentController::update/$1');
 $routes->get('/payment/delete/(:num)', 'paymentController::delete/$1');
 ///$routes->get('/myPayments', 'paymentController::myPayments');
 //$routes->get('/payment/edit', 'paymentController::edit');

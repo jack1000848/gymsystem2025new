be60f77e902a2fdeb466be10520dce434cc8051c
migs <taysonmiguelito125@gmail.com>
2025-04-20 18:43:45 +0800

    removing type of workout

--- app/Views/clients1crud/manage1.php
+++ app/Views/clients1crud/manage1.php
@@ -171,7 +171,6 @@ $this->section('body'); // Start the body section
                     <th>Email Address</th>
                     <th style="display:none;">Password</th>
                     <th>Register Date</th>
-                    <th>Types of Workout</th>
                     <th>Membership Plan</th>
                     <th>QR Code</th>
                     <th>Action</th>
@@ -188,7 +187,6 @@ $this->section('body'); // Start the body section
                         <td><?= $client['Email']; ?></td>
                         <td style="display:none;"><?= $client['password_hash']; ?></td>
                         <td><?= $client['RegisteredDate']; ?></td>
-                        <td><?= $client['types_of_workout']; ?></td>
                         <td><?= $client['Membership_plan']; ?></td>
                         <td><img id="qrCodeImage<?= $client['CustomerID']; ?>" src="" alt="QR Code" style="width: 100px;"></td>
                         <td>

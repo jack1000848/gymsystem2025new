7bebd4ae0818c38550babb7643dbf21ff7a29772
migs <taysonmiguelito125@gmail.com>
2025-04-20 16:55:51 +0800

    try1

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -142,7 +142,7 @@ class CustomerController extends BaseController
         $insertClients = new CustomerModel ();
 
         // Retrieve the email from the form input
-    $email = $this->request->getPost('clients1Emailaddress');
+   /// $email = $this->request->getPost('clients1Emailaddress');
 
     // Check if email is retrieved properly
     if (empty($email)) {
@@ -217,7 +217,7 @@ class CustomerController extends BaseController
         
     }}
     private function sendVerificationEmail($email, $token)
-    {
+   {
         $emailService = service('email');
     
         $emailService->setTo($email);

c33dadbd0c8f3a8d29484db245b06efa53917be8
migs <taysonmiguelito125@gmail.com>
2025-04-21 01:24:35 +0800

    4.1

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -173,19 +173,18 @@ $this->section('body');
                     </svg>
                 </div>
             </div>
-        </div><div class="col-lg-3 col-6">
-                <div class="small-box text-bg-danger">
-                    <div class="inner">
-                        <h3><?= $activeMembers ?><sup class="fs-5"></sup></h3>
-                        <p>Active Members</p>
-                    </div>
-                    <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-                    </svg>
-                </div>
-            </div>
+            <div class="col-lg-3 col-6">
+            <div class="small-box text-bg-danger">
+        <div class="inner">
+            <h3><?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
+            <p>Total Expense for Equipment</p>
         </div>
+        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
+            <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
+        </svg>
+    </div>
+</div>
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">

b375327132c3359a82bac1044a7d314e2bd20286
migs <taysonmiguelito125@gmail.com>
2025-04-21 01:40:23 +0800

    try 1

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -174,15 +174,14 @@ $this->section('body');
                 </div>
             </div>
             <div class="col-lg-3 col-6">
-            <div class="small-box text-bg-danger">
+    <div class="small-box text-bg-danger">
         <div class="inner">
-            <h3><?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
+            <h3>₱<?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
             <p>Equipment Expenses</p>
         </div>
-        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
-            <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
-        </svg>
+        <div class="icon position-absolute top-0 end-0 p-3" style="opacity: 0.3;">
+            <i class="fas fa-money-bill-wave fa-3x"></i>
+        </div>
     </div>
 </div>
 

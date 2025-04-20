254cb5b5e8c144dc75fdace7347956a1143d835c
migs <taysonmiguelito125@gmail.com>
2025-04-21 01:22:55 +0800

    3.3

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -162,7 +162,7 @@ $this->section('body');
                 
             </div>
             <div class="col-lg-3 col-6">
-                <div class="bg-orange text-white">
+                <div class="small-box bg-orange text-white">
                     <div class="inner">
                         <h3><?= $activeMembers ?><sup class="fs-5"></sup></h3>
                         <p>Active Members</p>

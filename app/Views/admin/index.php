4959b6a129f9b34bd03bd946cf85eb8b207ad833
migs <taysonmiguelito125@gmail.com>
2025-04-21 01:17:54 +0800

    3

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -179,7 +179,7 @@ $this->section('body');
             <h3><?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
             <p>Total Expense for Equipment</p>
         </div>
-        <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <svg class="text-bg-light" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
             <path clip-rule="evenodd" fill-rule="evenodd" d="M12.75 3a.75.75 0 01.75-.75 8.25 8.25 0 018.25 8.25.75.75 0 01-.75.75h-7.5a.75.75 0 01-.75-.75V3z"></path>
         </svg>

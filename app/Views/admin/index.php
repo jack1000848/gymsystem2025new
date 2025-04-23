6f747c9374949ecd5701868cfe18975ac37d5d7e
migs <taysonmiguelito125@gmail.com>
2025-04-23 22:26:54 +0800

    1

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -187,8 +187,8 @@ $this->section('body');
                 <a href="<?= base_url('clients1') ?>" style="text-decoration: none; color: inherit; display: block;">
                     <div class="small-box text-bg-danger">
                         <div class="inner">
-                            <h3><?= $activeMembers ?><sup class="fs-5"></sup></h3>
-                            <p>Active Members</p>
+                        <h3>₱<?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
+                        <p>Equipment Expenses</p>
                         </div>
                         <svg class="small-box-icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2.25 13.5a8.25 8.25 0 018.25-8.25.75.75 0 01.75.75v6.75H18a.75.75 0 01.75.75 8.25 8.25 0 01-16.5 0z"></path>
@@ -203,9 +203,8 @@ $this->section('body');
                 <a href="<?= base_url('gymequipment') ?>" style="text-decoration: none; color: inherit; display: block;">
                     <div class="small-box" style="background-color: rgb(99, 224, 208);">
                         <div class="inner">
-                            <h3>₱<?= number_format($totalEquipmentExpense, 2) ?><sup class="fs-5"></sup></h3>
-                            <p>Equipment Expenses</p>
-                        </div>
+                        <h3><?= $activeMembers ?><sup class="fs-5"></sup></h3>
+                        <p>Active Members</p>
                         <div class="icon position-absolute top-0 end-0 p-3" style="opacity: 0.3;">
                             <i class="fas fa-money-bill-wave fa-3x"></i>
                         </div>

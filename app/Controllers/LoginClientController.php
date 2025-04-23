885dbe07ea4008e860c5ba3d551dbf77e4481a5b
migs <taysonmiguelito125@gmail.com>
2025-04-23 21:09:40 +0800

    3

--- app/Controllers/LoginClientController.php
+++ app/Controllers/LoginClientController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ClientloginModel;
 use App\Models\CustomerModel;
+use App\Models\LoginCoachModel;
 
     class LoginClientController extends BaseController
 {
